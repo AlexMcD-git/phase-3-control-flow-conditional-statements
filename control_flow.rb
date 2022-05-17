137fcf84038eca23a1e6b17418793947d89cd122
Alex McDaniel <alexpmcdaniel@gmail.com>
2022-05-17 03:01:55 -0400

    commit tree 391693b09af229efcf168daa737bf1176f487e93

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,44 @@
 def admin_login(username, password)
-  # your code here
+  if username == 'ADMIN' || username == 'admin'
+    if password == '12345'
+      return "Access granted"
+    end
+  end
+  "Access denied"
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  if temperature <40 
+     return "It's brisk out there!"
+  elsif temperature < 65 && temperature >= 40
+    return "It's a little chilly out there!"
+  elsif temperature >80 
+    return "It's too dang hot out there!"
+  else "It's perfect out there!"
+  end
 end
 
 def fizzbuzz(num)
-  # your code here
+  if num%3 == 0
+    if num%5 == 0
+    return "FizzBuzz"
+    end
+    return "Fizz"
+  elsif num%5 == 0
+    return "Buzz"
+  end
+  return num
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+  when '+' then num1+num2
+  when '-' then num1-num2
+  when '*' then num1*num2
+  when '/' then num1/num2
+  else 
+    puts'Invalid operation!' 
+    return nil
+  end
 end
 
